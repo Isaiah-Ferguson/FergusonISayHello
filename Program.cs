a4d5ca6ce67fad1470a314b4dcde045763575415
Isaiah-Ferguson <crestice@yahoo.com>
2022-10-18 08:42:41 -0700

    Finsihed

--- Program.cs
+++ Program.cs
@@ -2,23 +2,25 @@
 //10/17/22
 //Hello World
 string digits = "1234567890";
+string SpecialChar = "~!@#$%^&*()`[]{}|/?.,><";
 string input = "";
 string iinput = "";
 bool truinput = true;
 int trueint = 0;
 bool loop = true;
+
 while (loop == true)
-
-
 {
 Console.WriteLine("Hello, what is your name?");
 input = Console.ReadLine();
-
 truinput = Int32.TryParse(input, out trueint);
 
-if (input.IndexOfAny(digits.ToCharArray()) != -1){
+if (input.IndexOfAny(digits.ToCharArray()) != -1 || input.IndexOfAny(SpecialChar.ToCharArray()) != -1 )
+{
     Console.WriteLine("Unless your the product of Elon Musk and Grimes I don't think that's your name.");
-}else{
+}
+else
+{
     Console.WriteLine($"Hello {input} nice to meet you!");
 }
 
