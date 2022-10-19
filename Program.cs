a2717fe80840654f66c3d1236b37b6002fbd3bbe
Isaiah-Ferguson <crestice@yahoo.com>
2022-10-18 18:36:11 -0700

    Complete ready for Review

--- Program.cs
+++ Program.cs
@@ -1,7 +1,11 @@
-﻿//ISAIAH FERGUSON
-//10/17/22
+﻿//ISAIAH KEITH FERGUSON
+//10/18/22
 //Hello World
+//Will ask the user for their name, if the user has any numbers or special characters 
+//the program restarts, if not the program will print nice to meet you.
 //Peer Reviwed by:
+
+Console.Clear();
 
 string digits = "1234567890";
 string SpecialChar = "~!@#$%^&*()`[]{}|/?.,><";
@@ -17,20 +21,19 @@ Console.WriteLine("Hello, what is your name?");
 input = Console.ReadLine();
 truinput = Int32.TryParse(input, out trueint);
 
+if (truinput == false)
+{
 if (input.IndexOfAny(digits.ToCharArray()) != -1 || input.IndexOfAny(SpecialChar.ToCharArray()) != -1 )
 {
-    Console.WriteLine("Unless your the product of Elon Musk and Grimes I don't think that's your name.");
+    Console.WriteLine("Unless your the product of Elon Musk and Grimes or Prince, I don't think that's your name.");
 }
-else
-{
-    Console.WriteLine($"Hello {input} nice to meet you!");
+else{Console.WriteLine($"Hello {input} nice to meet you!");}
 }
+else{ Console.WriteLine("yea.... that aint it buddy. try again.");}
 
 Console.WriteLine("Please type End to end the program or Press enter to replay.");
 iinput = Console.ReadLine().ToLower();
 
-if (iinput == "end")
-{
-    loop = false;
-}
+if (iinput == "end"){loop = false;}
+
 }
